702173c5b36dadda2cd0d9dd68833420f17b3281
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-09 12:54:43 +0300

    add missing path to python3 executable

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def remove_char_at(str, n):
     new_str = ""
     for i in range(len(str)):
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_last_digit(number):
     digit = abs(number) % 10
     print(f"{digit}", end="")

f24c43764253e1ca1478002db0d67f033f3dea94
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-17 12:21:12 +0300

    check whether dictionary is empty before execution

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict) and a_dictionary:
         key, val = list(a_dictionary.items())[0]
         for k, v in a_dictionary.items():
             if v > val:

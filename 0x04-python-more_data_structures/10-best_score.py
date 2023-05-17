ccc523be0361b9879f54da28c64256aede31771c
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-17 11:16:15 +0300

    add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        key, val = list(a_dictionary.items())[0]
+        for k, v in a_dictionary.items():
+            if v > val:
+                val = v
+                key = k
+        return key
+    return None

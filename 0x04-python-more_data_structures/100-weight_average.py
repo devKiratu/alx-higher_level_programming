0439e84e3543d7394eb2d1e7d59dda1e62e328ec
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-17 12:42:18 +0300

    return 0 when list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return 0
         total_weight = 0
         weighted_score = 0
         for score, weight in my_list:
             weighted_score += score * weight
             total_weight += weight
         return weighted_score / total_weight
-    return 0

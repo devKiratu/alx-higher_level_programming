3c9d76693b9f8a96471dc294228b4f0c09a62cdc
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-29 12:10:31 +0300

    handle integer conversion explicitly

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        num = int(value)
+        print("{:d}".format(num))
         return True
     except ValueError:
         return False

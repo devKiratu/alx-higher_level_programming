bc18db9c5f36d32ed8a5ef7812eb171536e39f4b
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-29 17:58:09 +0300

    add 102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        if i > a:
+            raise Exception("Too far")
+        result += (a ** b) / i
+    else:
+        result = a + b
+    return result

91d04283eb39484b39cd9cd5f5a4fea20b738db3
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-30 11:36:51 +0300

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """ A class defining a square
+        Attributes:
+            __size: length of the side of the square
+    """
+    def __init__(self, size):
+        """ Parameters:
+                size: length of one side of the square
+        """
+        self.__size = size

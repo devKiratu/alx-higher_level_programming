6e12cc9872aa5fad93ffd9c87497175f960b4735
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-06-05 16:20:14 +0300

    replace type with isinstance for type checking

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
             Args:
                 value: the new value for width
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
             Args:
                 value: the new value for height
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")

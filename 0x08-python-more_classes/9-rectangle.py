1413c21e91bd64236aefb8570e41e56b6c3ef392
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-06-05 18:49:35 +0300

    reoder __dict__ properties

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
                 TypeError: if width/height is not an integer
                 ValueError: if width/height is less than zero
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property

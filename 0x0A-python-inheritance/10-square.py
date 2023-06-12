4f83cecb2da34a08c75577d52c66aa3111a53c47
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-06-13 01:30:15 +0300

    document __init__()

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """class representation for Square"""
     def __init__(self, size):
+        """initializes parent class constructor and internal attributes"""
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size

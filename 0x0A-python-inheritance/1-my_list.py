cc778e4489684549268d36045912d851de375cb9
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-06-12 17:46:53 +0300

    replace list.sort with sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,4 @@ class MyList(list):
     """Custom list class"""
     def print_sorted(self):
         """prints sorted list"""
-        cp = self[:]
-        cp.sort()
-        print("{}".format(cp))
+        print("{}".format(sorted(self)))

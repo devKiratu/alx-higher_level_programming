524b017265e005f623037bcb6f4aec98c19421f5
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-06-16 18:26:49 +0300

    add tests for square update method

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -31,3 +31,21 @@ class TestSquare(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             s1.size = "9"
+
+    def test_square_update(self):
+        s1 = Square(5, 0, 0, 1)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 5")
+        s1.update(10)
+        self.assertEqual(s1.__str__(), "[Square] (10) 0/0 - 5")
+        s1.update(1, 2)
+        self.assertEqual(s1.__str__(), "[Square] (1) 0/0 - 2")
+        s1.update(1, 2, 3)
+        self.assertEqual(s1.__str__(), "[Square] (1) 3/0 - 2")
+        s1.update(1, 2, 3, 4)
+        self.assertEqual(s1.__str__(), "[Square] (1) 3/4 - 2")
+        s1.update(x=12)
+        self.assertEqual(s1.__str__(), "[Square] (1) 12/4 - 2")
+        s1.update(size=7, y=1)
+        self.assertEqual(s1.__str__(), "[Square] (1) 12/1 - 7")
+        s1.update(size=7, id=89, y=1)
+        self.assertEqual(s1.__str__(), "[Square] (89) 12/1 - 7")

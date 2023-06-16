42b171dd3f737a547eb1deb507fada9070ce7fef
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-06-16 18:15:23 +0300

    add tests for square size property

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -22,3 +22,12 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s2.__str__(), "[Square] (2) 2/0 - 2")
         s3 = Square(3, 1, 3, 3)
         self.assertEqual(s3.__str__(), "[Square] (3) 1/3 - 3")
+
+    def test_size(self):
+        s1 = Square(5, 0, 0, 1)
+        self.assertEqual(s1.size, 5)
+        s1.size = 10
+        self.assertEqual(s1.size, 10)
+
+        with self.assertRaises(TypeError):
+            s1.size = "9"

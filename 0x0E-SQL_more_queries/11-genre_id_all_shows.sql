a487856325478acb735d2fe452b67ddb0316afb0
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-07-19 13:01:27 +0300

    update comments on 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- This script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- This script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked or none.
 SELECT ts.title, tsg.genre_id
 FROM tv_shows ts 
 LEFT JOIN tv_show_genres tsg 

2e94b5d363c4ccc7ee82847855ed09dd26f0c034
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-07-19 15:53:02 +0300

    add 103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+-- This script lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+SELECT tg.name, sum(tsr.rate) AS rating 
+FROM tv_genres tg 
+JOIN tv_show_genres tsg 
+ON tg.id = tsg.genre_id 
+JOIN tv_show_ratings tsr 
+ON tsg.show_id = tsr.show_id 
+GROUP BY tg.name 
+ORDER BY rating DESC;

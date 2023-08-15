10bc7626bb72fd4adf9048cb472159d6bee17cdf
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-15 10:09:40 +0300

    add 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;

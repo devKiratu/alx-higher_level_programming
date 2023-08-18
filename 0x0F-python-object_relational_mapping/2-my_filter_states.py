8f8b68dc9d95a0056067b8c31d4bdc9384776a5e
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-18 16:50:26 +0300

    enforce case sensitive comparison

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,10 @@ if __name__ == "__main__":
             host="localhost", port=3306, user=user, passwd=passwd, db=db,
             charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(state))
+    cur.execute(
+                "SELECT * FROM states\
+                 WHERE name LIKE BINARY '{}'".format(state)
+                )
     rows = cur.fetchall()
 
     for row in rows:

f658886ced186446ea9eb4d76984649f5697537c
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-20 15:15:25 +0300

    format code properly

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-contains the class definition of a State and an instance Base = declarative_base()
+contains the class definition of a State and an
+instance Base = declarative_base()
 """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """Mapping class for states table"""

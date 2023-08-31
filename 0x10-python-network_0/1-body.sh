4286a1b2fc5be71a2304b77030f735864b3dae70
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-31 11:18:11 +0300

    allow script to follow redirections

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of a 200 response
-curl -sI -o /dev/null "$1" -w "%{http_code}" | grep -q 200 && curl -s "$1" 
+curl -sIL -o /dev/null "$1" -w "%{http_code}" | grep -q 200 && curl -sL "$1" 

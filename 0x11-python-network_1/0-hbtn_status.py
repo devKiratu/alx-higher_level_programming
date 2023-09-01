e8347f62886234b0513d1561e7164d9b41148060
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-01 13:22:37 +0300

    prevent code execution on import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,11 +6,11 @@ from https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
-
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t - type: {}".format(type(body)))
-    print("\t - content: {}".format(body))
-    print("\t - utf8 content: {}".format(body.decode()))
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(body)))
+        print("\t - content: {}".format(body))
+        print("\t - utf8 content: {}".format(body.decode()))

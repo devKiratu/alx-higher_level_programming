ec264ad4b9d015408b85501035d0735b8521ec5f
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-01 13:25:50 +0300

    perform imports within the if guard

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,8 @@
 from https://alx-intranet.hbtn.io/status
 """
 
-
-import urllib.request
-
 if __name__ == "__main__":
+    import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()

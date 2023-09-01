b10d31e927a5d3dff56e13ecdea6276085b0de7a
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-01 14:33:45 +0300

    rectify encoding operation typo

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,8 +11,7 @@ import sys
 
 if __name__ == "__main__":
     values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = urllib.encode('ascii')
+    data = urllib.parse.urlencode(values).encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         body = response.read()

78d8e18a5c54644ec04e64bcc0800e0312a234d8
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-01 14:07:50 +0300

    remove redundant variables

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         headers = response.getheaders()

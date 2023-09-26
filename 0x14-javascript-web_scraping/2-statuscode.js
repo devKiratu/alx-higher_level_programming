bcf6ecc4861e64a418f8ce70738d362b5d75c48f
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-26 08:51:52 +0300

    add 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) { throw error; }
+  if (response) { console.log('code: ', response.statusCode); }
+});
